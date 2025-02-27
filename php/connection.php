340839bd418a69fde652295cabf7fe24768291aa
Winner Isac KODJO <kodjowinnerisac@gmail.com>
2025-02-27 19:51:24 +0100

    debut projet

--- php/connection.php
+++ php/connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "db5017292825.hosting-data.io";
-$username = "dbu827038";
-$password = "Kodjowinner2005$";
-$dbname = "dbs13870670";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
